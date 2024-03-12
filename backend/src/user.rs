ba6c58b222c4cbc578b66041e08162edd6f6483b
Pandier <firegox@email.cz>
2024-03-12 21:14:32 +0100

    refactor: Add public user model

--- backend/src/user.rs
+++ backend/src/user.rs
@@ -1,7 +1,23 @@
-use rocket::serde::Serialize;
+use rocket::serde::{Deserialize, Serialize};
+use surrealdb::sql::Thing;
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct User {
+    pub id: Thing,
+    pub email: String,
+}
 
 #[derive(Serialize)]
-#[serde(crate="rocket::serde")]
-pub struct User<'a> {
-    pub id: &'a str,
+#[serde(crate = "rocket::serde")]
+pub struct PublicUser {
+    pub id: String,
+}
+
+impl From<User> for PublicUser {
+    fn from(value: User) -> Self {
+        Self {
+            id: value.id.id.to_raw(),
+        }
+    }
 }
