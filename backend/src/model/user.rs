af3def96efea730de96dd7afc982aab31fe60818
Pandier <firegox@email.cz>
2024-03-15 23:13:01 +0100

    refactor: Add derive Debug to user models

--- backend/src/model/user.rs
+++ backend/src/model/user.rs
@@ -1,14 +1,14 @@
 use rocket::serde::{Deserialize, Serialize};
 use surrealdb::sql::Thing;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(crate = "rocket::serde")]
 pub struct User {
     pub id: Thing,
     pub email: String,
 }
 
-#[derive(Serialize)]
+#[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct PublicUser {
     pub id: String,
