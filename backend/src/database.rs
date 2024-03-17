ffdebdccd123af9af4b38742c55d2de260144da3
Pandier <firegox@email.cz>
2024-03-17 21:53:44 +0100

    feat: Use database auth in surrealdb

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -1,7 +1,7 @@
 use eyre::Result;
 use surrealdb::{
     engine::remote::ws::{Client, Ws},
-    opt::auth::Root,
+    opt::auth,
     Surreal,
 };
 
@@ -11,15 +11,12 @@ pub async fn init(settings: &Settings) -> Result<Surreal<Client>> {
     let database = Surreal::new::<Ws>("127.0.0.1:8000").await?;
 
     database
-        .signin(Root {
+        .signin(auth::Database {
             username: &settings.surreal_username,
             password: &settings.surreal_password,
+            namespace: &settings.surreal_namespace,
+            database: &settings.surreal_database,
         })
-        .await?;
-
-    database
-        .use_ns(&settings.surreal_namespace)
-        .use_db(&settings.surreal_database)
         .await?;
 
     Ok(database)
